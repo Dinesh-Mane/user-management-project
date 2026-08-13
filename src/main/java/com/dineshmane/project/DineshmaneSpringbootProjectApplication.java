d77f6342944f5b6eaf297df93c8f98ce6dafcd13
Dinesh <dinesh30mane@gmail.com>
2026-08-13 23:36:06 +0530

    feat: integrate ModelMapper library for DTO mapping

--- src/main/java/com/dineshmane/project/DineshmaneSpringbootProjectApplication.java
+++ src/main/java/com/dineshmane/project/DineshmaneSpringbootProjectApplication.java
@@ -1,10 +1,17 @@
 package com.dineshmane.project;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DineshmaneSpringbootProjectApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DineshmaneSpringbootProjectApplication.class, args);
